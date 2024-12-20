2a356d18af4ade50342e31576ef5d4d0f6f508f5
Mayur Wagh <mayur2002wagh@gmail.com>
2024-12-20 21:57:50 +0530

    userDetail cookie opt

--- Spread-Backend/utils/cookie-options.js
+++ Spread-Backend/utils/cookie-options.js
@@ -1,6 +1,6 @@
 export const CookieOptions = {
   httpOnly: true, // Prevents JavaScript access
   secure: process.env.NODE_ENV === "production", // Only for HTTPS
-  sameSite: "Strict", // Prevents cross-origin issues
+  sameSite: "none", // Prevents cross-origin issues
   maxAge: 3600000, // 1 hour
 };

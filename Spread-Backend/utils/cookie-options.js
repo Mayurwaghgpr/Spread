586046b02c3a0b4fde473d840124f191b1ba9531
Mayur Wagh <mayur2002wagh@gmail.com>
2024-12-20 19:35:18 +0530

    cookie sameSite to strict

--- Spread-Backend/utils/cookie-options.js
+++ Spread-Backend/utils/cookie-options.js
@@ -1,6 +1,6 @@
 export const CookieOptions = {
   httpOnly: true, // Prevents JavaScript access
   secure: process.env.NODE_ENV === "production", // Only for HTTPS
-  sameSite: "lex", // Prevents cross-origin issues
+  sameSite: "Strict", // Prevents cross-origin issues
   maxAge: 3600000, // 1 hour
 };

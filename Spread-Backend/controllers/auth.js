0995daea5d36f375fc65ac408980463754018230
Mayur Wagh <mayur2002wagh@gmail.com>
2024-12-20 19:45:47 +0530

    userDetail cookie opt

--- Spread-Backend/controllers/auth.js
+++ Spread-Backend/controllers/auth.js
@@ -1,240 +1,256 @@
-import User from '../models/user.js';
-import Sequelize, { where } from 'sequelize';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import dotenv from 'dotenv';
-import AccessAndRefreshTokenGenerator from '../utils/AccessAndRefreshTokenGenerator.js';
-import { mailTransporter } from '../utils/sendMail.js';
-import { CookieOptions } from '../utils/cookie-options.js';
+import User from "../models/user.js";
+import Sequelize, { where } from "sequelize";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import AccessAndRefreshTokenGenerator from "../utils/AccessAndRefreshTokenGenerator.js";
+import { mailTransporter } from "../utils/sendMail.js";
+import { CookieOptions } from "../utils/cookie-options.js";
 
 dotenv.config();
 const saltRounds = 10;
 
 // Sign up a new user
-export const SignUp = async (req, res,next) => {
-    const { username, email, password } = req.body;
-    // Validate required fields
-    if (!username || !email || !password) {
-        return res.status(400).json({ message: "All fields are required" });
+export const SignUp = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  // Validate required fields
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    // Check if user already exists
+    const existingUser = await User.findOne({
+      where: {
+        [Sequelize.Op.or]: [{ username }, { email }],
+      },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
 
-    try {
-        // Check if user already exists
-        const existingUser = await User.findOne({
-            where: {
-                [Sequelize.Op.or]: [{ username }, { email }],
-            },
-        });
+    // Hash the password and create a new user
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const newAddedUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+      userImage: "images/placeholderImages/ProfOutlook.png",
+    });
+    // Generate access and refresh tokens
+    const { AccessToken, RefreshToken } = AccessAndRefreshTokenGenerator({
+      id: newUser.id,
+      email: newUser.email,
+    });
 
-        if (existingUser) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-
-        // Hash the password and create a new user
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        const newAddedUser = await User.create({ username, email, password: hashedPassword,userImage:'images/placeholderImages/ProfOutlook.png' });
-        // Generate access and refresh tokens
-        const { AccessToken, RefreshToken } = AccessAndRefreshTokenGenerator({
-            id: newUser.id,
-            email: newUser.email,
-        });
-
-        if (!AccessToken || !RefreshToken) {
-            throw new Error("Failed to generate tokens");
-        }
-
-    const newUser = { ...newAddedUser.toJSON() }
-       delete newUser.password
-
-        // Set tokens as cookies and respond
-        res.status(201)
-            .cookie('AccessToken', AccessToken, CookieOptions)
-            .cookie('RefreshToken', RefreshToken, CookieOptions)
-            .cookie("_userDetail",newUser,{httpOnly:true})
-            .json({ user: newUser.dataValues, AccessToken, RefreshToken });
-
-    } catch (err) {
-        next(err)
-        console.error('Error during sign up:', err);
-        // res.status(500).json({ message: 'Server error' });
+    if (!AccessToken || !RefreshToken) {
+      throw new Error("Failed to generate tokens");
     }
+
+    const newUser = { ...newAddedUser.toJSON() };
+    delete newUser.password;
+
+    // Set tokens as cookies and respond
+    res
+      .status(201)
+      .cookie("AccessToken", AccessToken, CookieOptions)
+      .cookie("RefreshToken", RefreshToken, CookieOptions)
+      .cookie("_userDetail", newUser, { httpOnly: true })
+      .json({ user: newUser.dataValues, AccessToken, RefreshToken });
+  } catch (err) {
+    next(err);
+    console.error("Error during sign up:", err);
+    // res.status(500).json({ message: 'Server error' });
+  }
 };
 
 // Log in an existing user
-export const SignIn = async (req, res,next) => {
-    const { email, password } = req.body;
+export const SignIn = async (req, res, next) => {
+  const { email, password } = req.body;
 
-    // Validate required fields
-    if (!email || !password) {
-        return res.status(400).json({ message: "All fields are required" });
+  // Validate required fields
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    // Find user by username
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: User,
+          as: "Followers",
+          through: { attributes: { exclude: ["password"] } },
+        },
+        {
+          model: User,
+          as: "Following",
+          through: { attributes: { exclude: ["password"] } },
+        },
+      ],
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
     }
 
-    try {
-        // Find user by username
-        const user = await User.findOne({
-            where: { email },
-            include: [
-                {
-                    model: User,
-                    as: 'Followers',
-                    through: { attributes: { exclude: ['password'] }},
-                },
-                {
-                    model: User,
-                    as: 'Following',
-                    through: { attributes: { exclude: ['password'] } },
-                }
-            ]
-        });
+    // Compare provided password with hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
 
-        if (!user) {
-            return res.status(400).json({ message: 'User does not exist' });
-        }
-
-        // Compare provided password with hashed password
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if (!isMatch) {
-            return res.status(401).json({ message: 'Invalid credentials' });
-        }
-
-        // Generate access and refresh tokens
-        const { AccessToken, RefreshToken } = AccessAndRefreshTokenGenerator({
-            id: user.id,
-            email: user.email,
-        });
-
-        if (!AccessToken || !RefreshToken) {
-            throw new Error("Failed to generate tokens");
-        }
-
-        // Set tokens as cookies and respond
-        res.status(200)
-            .cookie('AccessToken', AccessToken, CookieOptions)
-            .cookie('RefreshToken', RefreshToken, CookieOptions)
-                       .cookie("_userDetail",user,{httpOnly:true})
-            .json({ user: user.dataValues, AccessToken, RefreshToken });
-    } catch (err) {
-        console.error('Error during login:', err);
-        next(err)
-        // res.status(500).json({ message: err.message });
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
+
+    // Generate access and refresh tokens
+    const { AccessToken, RefreshToken } = AccessAndRefreshTokenGenerator({
+      id: user.id,
+      email: user.email,
+    });
+
+    if (!AccessToken || !RefreshToken) {
+      throw new Error("Failed to generate tokens");
+    }
+
+    // Set tokens as cookies and respond
+    res
+      .status(200)
+      .cookie("AccessToken", AccessToken, CookieOptions)
+      .cookie("RefreshToken", RefreshToken, CookieOptions)
+      .cookie("_userDetail", user, { httpOnly: true })
+      .json({ user: user.dataValues, AccessToken, RefreshToken });
+  } catch (err) {
+    console.error("Error during login:", err);
+    next(err);
+    // res.status(500).json({ message: err.message });
+  }
 };
-
-
-
 
 // Refresh access token using refresh token
-export const RefreshToken = async (req, res,next) => {
-    // Extract token from cookies or Authorization header
-    const clientRefreshToken = req.cookies.RefreshToken || req.header("Authorization")?.replace("Bearer ", "");
+export const RefreshToken = async (req, res, next) => {
+  // Extract token from cookies or Authorization header
+  const clientRefreshToken =
+    req.cookies.RefreshToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
 
-    if (!clientRefreshToken) {
-        return res.status(401).json({ message: 'Unauthorized request' });
+  if (!clientRefreshToken) {
+    return res.status(401).json({ message: "Unauthorized request" });
+  }
+
+  try {
+    // Verify refresh token
+    const decodedToken = jwt.verify(
+      clientRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    const { dataValues: user } = await User.findByPk(decodedToken.id);
+    // console.log(user)
+    // console.log(decodedToken)
+    if (!user) {
+      return res.status(401).json({ message: "Invalid refresh token" });
     }
 
-    try {
-        // Verify refresh token
-        const decodedToken = jwt.verify(clientRefreshToken,  process.env.REFRESH_TOKEN_SECRET);
-        const { dataValues: user } = await User.findByPk(decodedToken.id);
-        // console.log(user)
-        // console.log(decodedToken)
-        if (!user) {
-            return res.status(401).json({ message: 'Invalid refresh token' });
-        }
-
-        // Check if the refresh token matches the one stored in the database
-        if (clientRefreshToken !== user.refreshToken) {
-            return res.status(401).json({ message: 'Refresh token is expired or used' });
-        }
-
-        // Generate new access and refresh tokens
-        const { AccessToken, RefreshToken } = AccessAndRefreshTokenGenerator({ id: decodedToken.id,    email: decodedToken.email});
-
-        // Update user with new refresh token
-        // await User.update({ refreshToken: RefreshToken }, { where: decodedToken.id });
-
-        // Set new tokens as cookies and respond
-        res.status(200)
-            .cookie('AccessToken', AccessToken, CookieOptions)
-            .cookie('RefreshToken', RefreshToken, CookieOptions)
-                       .cookie("_userDetail",user,{httpOnly:true})
-            .json({ user: user, AccessToken, RefreshToken });
-
-    } catch (error) {
-        console.error('Error during token refresh:', error);
-        next(error)
-        // res.status(500).json({ message: 'Internal server error' });
+    // Check if the refresh token matches the one stored in the database
+    if (clientRefreshToken !== user.refreshToken) {
+      return res
+        .status(401)
+        .json({ message: "Refresh token is expired or used" });
     }
+
+    // Generate new access and refresh tokens
+    const { AccessToken, RefreshToken } = AccessAndRefreshTokenGenerator({
+      id: decodedToken.id,
+      email: decodedToken.email,
+    });
+
+    // Update user with new refresh token
+    // await User.update({ refreshToken: RefreshToken }, { where: decodedToken.id });
+
+    // Set new tokens as cookies and respond
+    res
+      .status(200)
+      .cookie("AccessToken", AccessToken, CookieOptions)
+      .cookie("RefreshToken", RefreshToken, CookieOptions)
+      .cookie("_userDetail", user, { httpOnly: true })
+      .json({ user: user, AccessToken, RefreshToken });
+  } catch (error) {
+    console.error("Error during token refresh:", error);
+    next(error);
+    // res.status(500).json({ message: 'Internal server error' });
+  }
 };
-
 
 // Log out the user by clearing cookies and updating user record
 export const Logout = async (req, res) => {
-    res.clearCookie('AccessToken', CookieOptions)
-    .clearCookie('RefreshToken', CookieOptions)
-    .clearCookie("_userDetail",{httpOnly:true})
-    try {
-        // Clear refresh token from user record
-        await User.update({ refreshToken: null }, { where: { id: req.authUser.id } });
-        res.json({ message: 'Logged out successfully' });
-    } catch (error) {
-        console.error('Error during logout:', error);
-        // res.status(500).json({ message: 'Server error' });
-        next(error)
-
-    }
+  res
+    .clearCookie("AccessToken", CookieOptions)
+    .clearCookie("RefreshToken", CookieOptions)
+    .clearCookie("_userDetail", { httpOnly: true });
+  try {
+    // Clear refresh token from user record
+    await User.update(
+      { refreshToken: null },
+      { where: { id: req.authUser.id } }
+    );
+    res.json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.error("Error during logout:", error);
+    // res.status(500).json({ message: 'Server error' });
+    next(error);
+  }
 };
 
-
-
-export const forgotPass = async (req,res,next) => {
-    const { email } = req.body;
-try {
-    
-        const user = await User.findOne({ where: { email: email } });
+export const forgotPass = async (req, res, next) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ where: { email: email } });
     // console.log(user)
-        if (!user) {
-            res.status(404).json({ message: 'user not Found' });
-            return;
-        }
-        
-        const {AccessToken}=AccessAndRefreshTokenGenerator({email})
-        
-       const mail=await mailTransporter.sendMail({
-           to: email,
-           
-            subject: 'Reset password for ...Spread',
-            html:`<p>Click the link below to reset your password:</p>
+    if (!user) {
+      res.status(404).json({ message: "user not Found" });
+      return;
+    }
+
+    const { AccessToken } = AccessAndRefreshTokenGenerator({ email });
+
+    const mail = await mailTransporter.sendMail({
+      to: email,
+
+      subject: "Reset password for ...Spread",
+      html: `<p>Click the link below to reset your password:</p>
         <a href="${process.env.FRONT_END_URL}Resetpassword/${AccessToken}" style="color: #1a73e8; text-decoration: none;">
             Reset Password
         </a>
         <p>If you did not request this, please ignore this email.</p>`,
-       })
-    
-    res.status(200).json({success:"Mail successfuly sent to "+mail.messageId})
-        
-} catch (error) {
-    next(error)
-}
-    
-}
+    });
 
-
-export const resetPassword = async (req,res,next) => {
-    const token = req.params.token;
-    const newpassword = req.body.password
-// console.log(token)
-  try {
-      let decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        if (decodeToken.exp * 1000 < Date.now()) {
-              return res.status(401).json({ message: 'token has expired ,cannot Reset password' });
-        }
-      const email = decodeToken.email;
-        const hashedPassword = await bcrypt.hash(newpassword, saltRounds);
-       await User.update({password:hashedPassword},{ where: { email: email } });
-      res.status(200).json({success:'Password updated successfuly'})
+    res
+      .status(200)
+      .json({ success: "Mail successfuly sent to " + mail.messageId });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+
+export const resetPassword = async (req, res, next) => {
+  const token = req.params.token;
+  const newpassword = req.body.password;
+  // console.log(token)
+  try {
+    let decodeToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    if (decodeToken.exp * 1000 < Date.now()) {
+      return res
+        .status(401)
+        .json({ message: "token has expired ,cannot Reset password" });
+    }
+    const email = decodeToken.email;
+    const hashedPassword = await bcrypt.hash(newpassword, saltRounds);
+    await User.update(
+      { password: hashedPassword },
+      { where: { email: email } }
+    );
+    res.status(200).json({ success: "Password updated successfuly" });
+  } catch (error) {
+    next(error);
+  }
+};
--- Spread-Backend/controllers/mediaAuth.js
+++ Spread-Backend/controllers/mediaAuth.js
@@ -1,43 +1,41 @@
-
-import dotenv from 'dotenv';
-import AccessAndRefreshTokenGenerator from '../utils/AccessAndRefreshTokenGenerator.js';
-import { CookieOptions } from '../utils/cookie-options.js';
+import dotenv from "dotenv";
+import AccessAndRefreshTokenGenerator from "../utils/AccessAndRefreshTokenGenerator.js";
+import { CookieOptions } from "../utils/cookie-options.js";
 
 dotenv.config();
 
-
 export const googleAuth = async (req, res, next) => {
-    const user = req.user;
-    try {
-        const { AccessToken, RefreshToken } = AccessAndRefreshTokenGenerator({
-            id: user.id,
-            email: user.email,
-        });
+  const user = req.user;
+  try {
+    const { AccessToken, RefreshToken } = AccessAndRefreshTokenGenerator({
+      id: user.id,
+      email: user.email,
+    });
 
-        res
-            .cookie('AccessToken', AccessToken, CookieOptions)
-            .cookie('RefreshToken', RefreshToken, CookieOptions)
-                       .cookie("_userDetail",user,{httpOnly:true})
-            .redirect(process.env.FRONT_END_URL);
-    } catch (error) {
-        next(error);
-    }
+    res
+      .cookie("AccessToken", AccessToken, CookieOptions)
+      .cookie("RefreshToken", RefreshToken, CookieOptions)
+      .cookie("_userDetail", user, CookieOptions)
+      .redirect(process.env.FRONT_END_URL);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const gitHubAuth = async(req,res,next) => {
-         const user = req.user;
-    try {
+export const gitHubAuth = async (req, res, next) => {
+  const user = req.user;
+  try {
+    const { AccessToken, RefreshToken } = AccessAndRefreshTokenGenerator({
+      id: user.id,
+      email: user.email,
+    });
 
-        const { AccessToken, RefreshToken } = AccessAndRefreshTokenGenerator({
-            id: user.id,
-            email: user.email,
-        });
-
-        res.cookie('AccessToken', AccessToken, CookieOptions)
-            .cookie('RefreshToken', RefreshToken, CookieOptions)
-            .cookie("_userDetail",user,{httpOnly:true})
-            .redirect(process.env.FRONT_END_URL);
-    } catch (error) {
-        next(error);
-    }
-}
+    res
+      .cookie("AccessToken", AccessToken, CookieOptions)
+      .cookie("RefreshToken", RefreshToken, CookieOptions)
+      .cookie("_userDetail", user, { httpOnly: true })
+      .redirect(process.env.FRONT_END_URL);
+  } catch (error) {
+    next(error);
+  }
+};

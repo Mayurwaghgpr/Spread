5b0075c1c311fb418c68c02291ff9524052889a8
Mayur Wagh <mayur2002wagh@gmail.com>
2025-02-07 18:58:28 +0530

    redis fixes

--- Spread-Backend/utils/redisClient.js
+++ Spread-Backend/utils/redisClient.js
@@ -1,14 +1,18 @@
-// redisClient.js
-import dotenv from"dotenv"
-import {createClient} from "redis";
+import dotenv from "dotenv";
+import { createClient } from "redis";
+
 dotenv.config();
-const redisClient = createClient( process.env.NODE_ENV === "production"&&{url:process.env.REDIS_URL,  socket: {
-    tls: true, // Required for Render Redis
-  }});
 
+const redisOptions = process.env.NODE_ENV === "production"
+  ? { url: process.env.REDIS_URL, socket: { tls: true } }
+  : {};
 
-redisClient.on('error', err => console.log('Redis Client Error', err));
+const redisClient = createClient(redisOptions);
+
+redisClient.on("error", (err) => console.error("Redis Client Error:", err));
 
 await redisClient.connect();
+
+console.log("Connected to Redis");
 
 export default redisClient;

6aa2814a6dd5088505f9bad0343475573ba5ff50
Mayur Wagh <mayur2002wagh@gmail.com>
2025-07-02 09:34:13 +0530

    cookies option added

--- server/utils/cookie-options.js
+++ server/utils/cookie-options.js
@@ -3,6 +3,6 @@ dotenv.config();
 export const CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? 'none' : 'Strict',
-  maxAge:7*24*60*60*1000
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Strict",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
 };

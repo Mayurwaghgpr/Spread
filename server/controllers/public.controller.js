ef2ca979c354e000e1ce2b01375b0a03e7da629c
Mayur Wagh <mayur2002wagh@gmail.com>
2025-07-02 10:57:32 +0530

    removed import startedFollowing from public controller

--- server/controllers/public.controller.js
+++ server/controllers/public.controller.js
@@ -1,4 +1,4 @@
-import sequelize, { Op} from "sequelize";
+import sequelize, { Op } from "sequelize";
 import User from "../models/user.js";
 import Post from "../models/posts.js";
 import Follow from "../models/Follow.js";
@@ -6,8 +6,7 @@ import Archive from "../models/Archive.js";
 import Likes from "../models/Likes.js";
 import redisClient from "../utils/redisClient.js";
 import { EXPIRATION } from "../config/constants.js";
-import { startedFollowing } from "../services/notification.js";
-
+// import { startedFollowing } from "../services/notification.js";
 
 // Fetch all users except the current user and distinct topics
 export const getHomeContent = async (req, res, next) => {
@@ -30,7 +29,14 @@ export const getHomeContent = async (req, res, next) => {
         },
       ],
 
-      attributes: ["id", "username", "userImage", "bio", "displayName", "createdAt"],
+      attributes: [
+        "id",
+        "username",
+        "userImage",
+        "bio",
+        "displayName",
+        "createdAt",
+      ],
       order: [[sequelize.fn("RANDOM")]], // Random order
       limit: 4,
     });
@@ -53,7 +59,7 @@ export const getPeoples = async (req, res, next) => {
   const lastTimestamp = req.query.lastTimestamp || new Date().toISOString();
   const currentUserId = req.authUser.id;
   const searchQuery = req.query.q || "";
-  console.log("peoples")
+  console.log("peoples");
   try {
     // const cacheKey = `find_peoples_${currentUserId}_${searchQuery}_${lastTimestamp}_${limit}`;
 
@@ -66,53 +72,53 @@ export const getPeoples = async (req, res, next) => {
     // }
     // console.log('cach miss')
 
-const peoples = await User.findAll({
-  include: [
-    {
-      model: User,
-      as: "Followers",
-      through: { attributes: [] },
-      attributes: ["id"],
-    },
-    {
-      model: User,
-      as: "Following",
-      through: { attributes: [] },
-      attributes: ["id"],
-    },
-  ],
-  attributes: ["id", "displayName", "username", "userImage", "createdAt"],
-  where: {
-    username: {
-      [Op.or]: [
-        { [Op.iLike]: `${searchQuery}%` },
-        { [Op.iLike]: `%${searchQuery}%` },
-        { [Op.iLike]: `${searchQuery}` },
+    const peoples = await User.findAll({
+      include: [
+        {
+          model: User,
+          as: "Followers",
+          through: { attributes: [] },
+          attributes: ["id"],
+        },
+        {
+          model: User,
+          as: "Following",
+          through: { attributes: [] },
+          attributes: ["id"],
+        },
       ],
-    },
-    createdAt: { [Op.lt]: lastTimestamp },
-  },
-  order: [["createdAt", "DESC"]], // IMPORTANT
-  limit,
-});
-      // await redisClient.setEx(cacheKey,EXPIRATION,JSON.stringify(peoples))
-   res.status(200).json({peoples});
+      attributes: ["id", "displayName", "username", "userImage", "createdAt"],
+      where: {
+        username: {
+          [Op.or]: [
+            { [Op.iLike]: `${searchQuery}%` },
+            { [Op.iLike]: `%${searchQuery}%` },
+            { [Op.iLike]: `${searchQuery}` },
+          ],
+        },
+        createdAt: { [Op.lt]: lastTimestamp },
+      },
+      order: [["createdAt", "DESC"]], // IMPORTANT
+      limit,
+    });
+    // await redisClient.setEx(cacheKey,EXPIRATION,JSON.stringify(peoples))
+    res.status(200).json({ peoples });
   } catch (error) {
     console.error("Error fetching peoples data:", error);
     next(error);
   }
-  
-  
-}
+};
 // Search for posts based on a query string
 export const searchData = async (req, res, next) => {
   const searchQuery = req.query.q;
 
   try {
-       const cachedSearchData = await redisClient.get(searchQuery);
+    const cachedSearchData = await redisClient.get(searchQuery);
     if (cachedSearchData !== null) {
-      console.log('cach hit')
-      return res.status(200).json({key:searchQuery,data:JSON.parse(cachedSearchData)});// Send cached data
+      console.log("cach hit");
+      return res
+        .status(200)
+        .json({ key: searchQuery, data: JSON.parse(cachedSearchData) }); // Send cached data
     }
     const searchResult = await Post.findAll({
       where: {
@@ -140,47 +146,50 @@ export const searchData = async (req, res, next) => {
       // attributes: [],
       limit: 10,
     });
-    if(searchResult.length>0){await redisClient.setEx(searchQuery,EXPIRATION,JSON.stringify(searchResult))}
-    res.status(200).json({key:searchQuery,data:searchResult});
+    if (searchResult.length > 0) {
+      await redisClient.setEx(
+        searchQuery,
+        EXPIRATION,
+        JSON.stringify(searchResult)
+      );
+    }
+    res.status(200).json({ key: searchQuery, data: searchResult });
   } catch (error) {
     console.error("Error searching data:", error);
     next(error);
   }
 };
 
-export const getAllUser = async (req,res,next) => {
+export const getAllUser = async (req, res, next) => {
   const limit = Math.max(parseInt(req.query.limit?.trim()) || 5, 1);
   const lastTimestamp = req.query.lastTimestamp || new Date().toISOString();
   const currentUserId = req.authUser.id;
-  console.log(limit)
+  console.log(limit);
   try {
     const cacheKey = `Users_Data_${lastTimestamp}__${limit}`;
     // Unique cache key for this combination
     // Checking Cache
     const cachedData = await redisClient.get(cacheKey);
     if (cachedData !== null) {
-      console.log('cach hit')
-      return res.status(200).json(JSON.parse(cachedData));// Send cached data
+      console.log("cach hit");
+      return res.status(200).json(JSON.parse(cachedData)); // Send cached data
     }
     const users = await User.findAll({
       where: {
         id: { [Op.ne]: currentUserId },
-        createdAt: { [Op.lt]: lastTimestamp }
+        createdAt: { [Op.lt]: lastTimestamp },
       },
-      attributes: ['id', 'displayName', 'username', 'userImage'],
-      order: [
-        ["createdAt", "ASC"],
-      ],
-      limit, 
-    })
-    await redisClient.setEx(cacheKey,EXPIRATION,JSON.stringify(users))
-    res.status(200).json(users)
+      attributes: ["id", "displayName", "username", "userImage"],
+      order: [["createdAt", "ASC"]],
+      limit,
+    });
+    await redisClient.setEx(cacheKey, EXPIRATION, JSON.stringify(users));
+    res.status(200).json(users);
   } catch (error) {
     console.error("Error fetching users data:", error);
     next(error);
   }
-  
-}
+};
 
 export const LikePost = async (req, res, next) => {
   const { postId, liketype: type } = req.body;
@@ -191,12 +200,11 @@ export const LikePost = async (req, res, next) => {
     const existingLike = await Likes.findOne({ where: { likedBy, postId } });
 
     if (existingLike) {
-      !type ?
-        // If type is not provided, remove the like
-        await existingLike.destroy() :
-        // If type is provided, update the like type
-        await existingLike.update({ type });
-
+      !type
+        ? // If type is not provided, remove the like
+          await existingLike.destroy()
+        : // If type is provided, update the like type
+          await existingLike.update({ type });
     } else {
       // If the like doesn't exist, create a new one
       await Likes.create({ likedBy, postId, type });
@@ -207,7 +215,11 @@ export const LikePost = async (req, res, next) => {
 
     // Send response
     res.status(201).json({
-      message: existingLike ? (type ? "like updated" : "removed like") : "added like",
+      message: existingLike
+        ? type
+          ? "like updated"
+          : "removed like"
+        : "added like",
       updatedLikes,
     });
   } catch (error) {
@@ -242,17 +254,22 @@ export const FollowUser = async (req, res, next) => {
     if (existingFollow) {
       // Unfollow user
       await existingFollow.destroy();
-      userInfo.Following = userInfo.Following.filter((follow) => follow.id !== followedId);
+      userInfo.Following = userInfo.Following.filter(
+        (follow) => follow.id !== followedId
+      );
     } else {
       // Follow user
       await Follow.create({ followerId, followedId });
       userInfo.Following = [...userInfo.Following, { id: followedId }];
-       startedFollowing(followedId, followerId)
+      //  startedFollowing(followedId, followerId)
     }
-   
-    res
-      .status(201)
-      .json({ status: "success", message: existingFollow ? "Unfollowed successfully" : "Followed successfully" });
+
+    res.status(201).json({
+      status: "success",
+      message: existingFollow
+        ? "Unfollowed successfully"
+        : "Followed successfully",
+    });
   } catch (error) {
     console.error("Error in FollowUser:", error);
     next(error);
@@ -264,31 +281,33 @@ export const AddPostToArchive = async (req, res, next) => {
   const { postId } = req.body;
   const userId = req.authUser.id;
 
-  const userInfo = JSON.parse(await redisClient.get(userId))
+  const userInfo = JSON.parse(await redisClient.get(userId));
   try {
     let updatedUserInfo;
     let message;
     const exist = await Archive.findOne({ where: { postId, userId } });
     if (exist) {
-      const filterSavedPost = userInfo?.savedPosts?.filter(post => post.id !== postId)
-      if (Array.isArray(filterSavedPost)) { 
-        updatedUserInfo = { ...userInfo, savedPosts: filterSavedPost }
-    }
+      const filterSavedPost = userInfo?.savedPosts?.filter(
+        (post) => post.id !== postId
+      );
+      if (Array.isArray(filterSavedPost)) {
+        updatedUserInfo = { ...userInfo, savedPosts: filterSavedPost };
+      }
       await exist.destroy();
-      message = "Removed from archive"
+      message = "Removed from archive";
     } else {
       await Archive.create({ postId, userId });
-      updatedUserInfo = { ...userInfo, savedPosts: [...userInfo.savedPosts, { id: postId }] }
-      message="Post archived successfully"
+      updatedUserInfo = {
+        ...userInfo,
+        savedPosts: [...userInfo.savedPosts, { id: postId }],
+      };
+      message = "Post archived successfully";
     }
-    await redisClient.setEx(userId,3600,JSON.stringify(updatedUserInfo))
-    res
-      .status(200)
-      .json({
-        message ,
-        archived: updatedUserInfo.savedPosts,
-      });
-
+    await redisClient.setEx(userId, 3600, JSON.stringify(updatedUserInfo));
+    res.status(200).json({
+      message,
+      archived: updatedUserInfo.savedPosts,
+    });
   } catch (error) {
     console.error("Error archiving post:", error);
     next(error);
